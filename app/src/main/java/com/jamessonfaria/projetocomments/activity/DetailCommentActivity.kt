9ce4f71803fcab4ffff815e9443076cf6fd6db56
Jamesson Faria <jamessonjr@gmail.com>
2018-06-26 23:24:32 -0300

    Adicionar mapa no detail, ajustado cores do icone e splash.

--- app/src/main/java/com/jamessonfaria/projetocomments/activity/DetailCommentActivity.kt
+++ app/src/main/java/com/jamessonfaria/projetocomments/activity/DetailCommentActivity.kt
@@ -1,16 +1,35 @@
 package com.jamessonfaria.projetocomments.activity
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.support.v4.app.ActivityCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.jamessonfaria.projetocomments.R
 import com.google.gson.reflect.TypeToken
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.jamessonfaria.projetocomments.R.id.*
 import com.jamessonfaria.projetocomments.adapter.AdapterComentarios
 import com.jamessonfaria.projetocomments.model.Comentario
 import com.jamessonfaria.projetocomments.util.Network
@@ -20,9 +39,14 @@ import kotlinx.android.synthetic.main.adapter_lista_comentarios.view.*
 import org.jetbrains.anko.*
 
 
-class DetailCommentActivity : AppCompatActivity() {
+class DetailCommentActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
     private var comentario: Comentario? = null
+    var mGoogleMap: GoogleMap? = null
+    private var net: Network? = null
+    protected var locationManager: LocationManager? = null
+    private var map: SupportMapFragment? = null
+    private val INITIAL_REQUEST = 200
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,6 +70,9 @@ class DetailCommentActivity : AppCompatActivity() {
                     .into(imgFoto)
 
         }
+
+        loadMap()
+
     }
 
     fun delete(view: View) {
@@ -93,4 +120,76 @@ class DetailCommentActivity : AppCompatActivity() {
         }
 
     }
+
+    public inline fun String.toDouble(): Double = java.lang.Double.parseDouble(this)
+
+    // ############## Metodos do Mapa
+
+    private fun loadMap() {
+
+        //locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager // pra obter a localização
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                    checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), INITIAL_REQUEST)
+            }
+            return
+        }
+        map = supportFragmentManager.findFragmentById(R.id.fragmentMapa) as SupportMapFragment
+        map!!.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap?) {
+        mGoogleMap = googleMap
+
+       // val latLng = LatLng(location!!.getLatitude(), location!!.getLongitude())
+
+        if(comentario?.lat != null && comentario?.lng != null && !comentario?.lat.equals("") && !comentario?.lng.equals("")){
+
+            val latLng = LatLng(comentario!!.lat.toDouble(), comentario!!.lng.toDouble())
+
+            mGoogleMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12.0f))
+            mGoogleMap!!.addMarker(MarkerOptions()
+                    .position(latLng)
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.mark)))
+            //mGoogleMap!!.setMyLocationEnabled(true)
+            mGoogleMap!!.getUiSettings().isMyLocationButtonEnabled = false
+            mGoogleMap!!.setMapType(GoogleMap.MAP_TYPE_NORMAL)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            200 -> {
+                if (grantResults.size == 2
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    map = supportFragmentManager.findFragmentById(R.id.fragmentMapa) as SupportMapFragment
+                    map!!.getMapAsync(this)
+                }
+            }
+        }
+    }
+
+    override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderEnabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderDisabled(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onLocationChanged(p0: Location?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
 }
