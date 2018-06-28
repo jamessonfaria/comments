85b2ded4ed010840c039b569bc566fbc39f272ec
Jamesson Faria <jamessonjr@gmail.com>
2018-06-28 00:50:53 -0300

    Adicionado camera e album para a tela criacao de comentario.

--- app/src/main/java/com/jamessonfaria/projetocomments/activity/CreateCommentsActivity.kt
+++ app/src/main/java/com/jamessonfaria/projetocomments/activity/CreateCommentsActivity.kt
@@ -5,20 +5,32 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.AlertDialog
 import android.app.ProgressDialog
+import android.content.ContentValues
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.media.ExifInterface
+import android.net.Uri
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.os.StrictMode
+import android.provider.MediaStore
 import android.provider.Settings
 import android.support.v4.app.ActivityCompat
 import android.view.View
 import android.widget.EditText
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.github.rodlibs.persistencecookie.PersistentCookieStore
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException
 import com.google.android.gms.common.GooglePlayServicesRepairableException
@@ -34,11 +46,18 @@ import com.jamessonfaria.projetocomments.R
 import com.jamessonfaria.projetocomments.model.Comentario
 import com.jamessonfaria.projetocomments.util.Network
 import com.jamessonfaria.projetocomments.util.Util
+import com.kbeanie.imagechooser.api.*
+import com.kbeanie.imagechooser.exceptions.ChooserException
 import kotlinx.android.synthetic.main.activity_create_comments.*
 import org.jetbrains.anko.*
 import org.json.JSONArray
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
 
-class CreateCommentsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
+class CreateCommentsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener, ImageChooserListener {
 
     var mGoogleMap: GoogleMap? = null
     private var net: Network? = null
@@ -47,6 +66,14 @@ class CreateCommentsActivity : AppCompatActivity(), OnMapReadyCallback, Location
     private val INITIAL_REQUEST = 200
     var latLng: LatLng? = null
     private val REQUEST_PLACE_PICKER = 2
+
+    internal var imageChooserManager: ImageChooserManager? = null
+    internal var imgPhoto: ImageView? = null
+    internal var img1Path = ""
+    private val INTENT_CAMERA = 19
+    internal var path_image: String? = null
+    internal var dataFoto: String? = null
+    internal var nameFoto: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -94,6 +121,183 @@ class CreateCommentsActivity : AppCompatActivity(), OnMapReadyCallback, Location
                 noButton { progress.cancel() }
             }.show()
         }
+    }
+
+    // ############## Metodos da Camera
+
+    fun takePicture(view: View) {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE), 100)
+        } else {
+            val builder = StrictMode.VmPolicy.Builder()
+            StrictMode.setVmPolicy(builder.build())
+
+            val folder = File(Environment.getExternalStorageDirectory().toString() + "/bichooser")
+            if (!folder.exists()) {
+                folder.mkdir()
+            }
+
+            val dateFormat = SimpleDateFormat("yyyyMMdd_HHmmss")
+            val dtt = java.util.Date()
+            dataFoto = dateFormat.format(dtt)
+            path_image = folder.absoluteFile.toString() + "/" + "Item" + "-" + dataFoto + ".jpg"
+
+            val f = File(Environment.getExternalStorageDirectory(), "temp.jpg")
+            val intent2 = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            intent2.putExtra(MediaStore.EXTRA_SCREEN_ORIENTATION, ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+            intent2.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f))
+            startActivityForResult(intent2, INTENT_CAMERA)
+        }
+    }
+
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+
+        if (requestCode == REQUEST_PLACE_PICKER) { // MAPA
+            if (resultCode == Activity.RESULT_OK) {
+                val place = PlaceAutocomplete.getPlace(this, data)
+                //                this.onPlaceSelected(place);
+
+                latLng = LatLng(place.latLng.latitude, place.latLng.longitude)
+
+                mGoogleMap!!.addMarker(MarkerOptions()
+                        .position(latLng!!)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.mark)))
+                mGoogleMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14.0f))
+
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                val status = PlaceAutocomplete.getStatus(this, data)
+            }
+        } else if (requestCode == INTENT_CAMERA && resultCode == Activity.RESULT_OK) { // CAMERA
+
+            val settings = getSharedPreferences("image", 0)
+            nameFoto = settings.getString("nameFoto", "")
+            //            alterou = true;
+
+            try {
+                val f = File(Environment.getExternalStorageDirectory(), "temp.jpg")
+
+                var ei: ExifInterface? = null
+                ei = ExifInterface(f.absolutePath)
+                val orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED)
+
+                var cameraBitmapOrig: Bitmap? = rotateImage(f.absolutePath, orientation)
+                System.gc()
+
+                val originalHeight = cameraBitmapOrig!!.height.toDouble()
+                val scale = originalHeight / 600
+
+                val wi = (cameraBitmapOrig.width / scale).toInt()
+                val hey = (cameraBitmapOrig.height / scale).toInt()
+                var cameraBitmapResized: Bitmap? = Bitmap.createScaledBitmap(cameraBitmapOrig, wi, hey, true)
+
+
+                try {
+                    f.delete()
+                    val folder = File(Environment.getExternalStorageDirectory().toString() + "/bichooser")
+                    if (!folder.exists()) {
+                        folder.mkdir()
+                    }
+
+                    val dateFormat = SimpleDateFormat("yyyyMMdd_HHmmss")
+                    val dtt = java.util.Date()
+                    dataFoto = dateFormat.format(dtt)
+                    path_image = folder.absoluteFile.toString() + "/" + "Item" + "-" + dataFoto + ".jpg"
+
+                    var fos: FileOutputStream? = FileOutputStream(path_image)
+                    cameraBitmapResized!!.compress(Bitmap.CompressFormat.JPEG, 80, fos)
+                    fos!!.flush()
+                    fos.close()
+                    fos = null
+                    cameraBitmapResized.recycle()
+                    cameraBitmapResized = null
+                    cameraBitmapOrig.recycle()
+                    cameraBitmapOrig = null
+
+
+                    System.gc()
+                    addImageToGallery(path_image!!, this@CreateCommentsActivity)
+
+
+
+                    imgPhoto!!.setVisibility(View.VISIBLE)
+                    img1Path = path_image as String
+                    Glide.with(this@CreateCommentsActivity).load(File(path_image)).into(imgPhoto)
+
+
+                } catch (e: FileNotFoundException) {
+
+                } catch (e: IOException) {
+
+                }
+
+            } catch (e: Exception) {
+
+            }
+
+        } else if (resultCode == Activity.RESULT_OK && requestCode == ChooserType.REQUEST_PICK_PICTURE) {
+            imageChooserManager!!.submit(requestCode, data)
+        }
+
+    }
+
+
+    fun rotateImage(photoPath: String, orientation: Int): Bitmap {
+        var angle = 90f
+
+        when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> angle = 90f
+            ExifInterface.ORIENTATION_ROTATE_180 -> angle = 180f
+            ExifInterface.ORIENTATION_ROTATE_270 -> angle = 270f
+            ExifInterface.ORIENTATION_UNDEFINED -> return BitmapFactory.decodeFile(photoPath)
+        }
+
+        val retVal: Bitmap
+        val source = BitmapFactory.decodeFile(photoPath)
+
+        val matrix = Matrix()
+        matrix.postRotate(angle)
+        retVal = Bitmap.createBitmap(source, 0, 0, source.width, source.height, matrix, true)
+        return retVal
+    }
+
+    fun addImageToGallery(filePath: String, context: Context) {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+        values.put(MediaStore.MediaColumns.DATA, filePath)
+        context.contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+    }
+
+    fun gallery(view: View) {
+        try {
+            imageChooserManager = ImageChooserManager(this@CreateCommentsActivity, ChooserType.REQUEST_PICK_PICTURE)
+            imageChooserManager!!.setImageChooserListener(this@CreateCommentsActivity)
+            imageChooserManager!!.choose()
+        } catch (e: ChooserException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    override fun onImageChosen(chosenImage: ChosenImage?) {
+        runOnUiThread {
+            if (chosenImage != null) {
+                imgPhoto!!.setVisibility(View.VISIBLE)
+                img1Path = chosenImage.getFilePathOriginal()
+                Glide.with(this@CreateCommentsActivity).load(File(chosenImage.getFilePathOriginal())).into(imgPhoto)
+            }
+        }
+    }
+
+    override fun onImagesChosen(p0: ChosenImages?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onError(p0: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     // ############## Metodos do Mapa
@@ -170,26 +374,26 @@ class CreateCommentsActivity : AppCompatActivity(), OnMapReadyCallback, Location
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if (requestCode == REQUEST_PLACE_PICKER) {
-            if (resultCode == Activity.RESULT_OK) {
-                val place = PlaceAutocomplete.getPlace(this, data)
-                //                this.onPlaceSelected(place);
-
-                latLng = LatLng(place.latLng.latitude, place.latLng.longitude)
-
-                mGoogleMap!!.addMarker(MarkerOptions()
-                        .position(latLng!!)
-                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.mark)))
-                mGoogleMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14.0f))
-
-            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
-                val status = PlaceAutocomplete.getStatus(this, data)
-            }
-        }
-    }
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        if (requestCode == REQUEST_PLACE_PICKER) {
+//            if (resultCode == Activity.RESULT_OK) {
+//                val place = PlaceAutocomplete.getPlace(this, data)
+//                //                this.onPlaceSelected(place);
+//
+//                latLng = LatLng(place.latLng.latitude, place.latLng.longitude)
+//
+//                mGoogleMap!!.addMarker(MarkerOptions()
+//                        .position(latLng!!)
+//                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.mark)))
+//                mGoogleMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 14.0f))
+//
+//            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+//                val status = PlaceAutocomplete.getStatus(this, data)
+//            }
+//        }
+//    }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         when (requestCode) {
@@ -218,8 +422,6 @@ class CreateCommentsActivity : AppCompatActivity(), OnMapReadyCallback, Location
     }
 
     override fun onLocationChanged(location: Location?) {
-        if (location != null) {
-            toast("chegueiiiii " + location.latitude + " " + location.longitude)
-        }
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
